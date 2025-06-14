b899c6d49b73a2fe63d1dea316f574c6721db4da
Sugihey <sugihey@gmail.com>
2025-06-14 12:51:24 +0000

    各項目のnullもしくは初期値0対応

--- app/UseCase/OrderUseCase.php
+++ app/UseCase/OrderUseCase.php
@@ -14,19 +14,21 @@ class OrderUseCase
             $orderNo = Order::generateOrderNo();
             
             $order = Order::create([
+                'title' => $request->title,
                 'billing_destination_id' => $request->billing_destination_id,
                 'order_type' => $request->order_type,
+                'priority' => $request->priority,
                 'billing_destination_name' => $request->billing_destination_name,
                 'property_name' => $request->property_name,
                 'property_address' => $request->property_address,
-                'room_no' => $request->room_no,
+                'room_no' => $request->room_no ?: null,
                 'order_date' => $request->order_date,
                 'deadline' => $request->deadline,
                 'is_photo_required' => $request->boolean('is_photo_required'),
                 'is_call_required' => $request->boolean('is_call_required'),
-                'resident_name' => $request->resident_name,
-                'resident_phone_no' => $request->resident_phone_no,
-                'assign_deadline' => $request->assign_deadline,
+                'resident_name' => $request->resident_name ?: null,
+                'resident_phone_no' => $request->resident_phone_no ?: null,
+                'assign_deadline' => $request->assign_deadline ?: null,
                 'jurisdiction' => $request->jurisdiction,
                 'order_no' => $orderNo,
             ]);
@@ -37,9 +39,9 @@ class OrderUseCase
                     'index' => $index + 1,
                     'operation_id' => $detailData['operation_id'],
                     'artisan_id' => $detailData['artisan_id'] ?: null,
-                    'quantity' => $detailData['quantity'],
-                    'incoming_order_price' => $detailData['incoming_order_price'],
-                    'purchase_order_price' => $detailData['purchase_order_price'],
+                    'quantity' => $detailData['quantity'] ?: 0,
+                    'incoming_order_price' => $detailData['incoming_order_price'] ?: 0,
+                    'purchase_order_price' => $detailData['purchase_order_price'] ?: 0,
                 ]);
             }
 
